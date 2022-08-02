46bb9ac5af13eb59edb6da78d438eb58fe29a28b
Carlos Augusto <caarlos.san@gmail.com>
2022-08-02 19:11:41 -0300

    Requisito 2 - Falta validar PropTypes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Album from './pages/Album';
 import Favorites from './pages/Favorites';
 import Login from './pages/Login';
@@ -9,17 +9,24 @@ import ProfileEdit from './pages/ProfileEdit';
 import Search from './pages/Search';
 
 class App extends React.Component {
+  // constructor() {
+  //   super();
+
+  // }
+
   render() {
     return (
       <div>
         <h2>TrybeTunes</h2>
-        <Route exact path="/" component={ Login } />
-        <Route exact path="/search" component={ Search } />
-        <Route exact path="/album/:id" component={ Album } />
-        <Route exact path="/favorites" component={ Favorites } />
-        <Route exact path="/profile" component={ Profile } />
-        <Route exact path="/profile/edit" component={ ProfileEdit } />
-        <Route exact path="*" component={ NotFound } />
+        <Switch>
+          <Route exact path="/" component={ Login } />
+          <Route exact path="/search" component={ Search } />
+          <Route exact path="/album/:id" component={ Album } />
+          <Route exact path="/favorites" component={ Favorites } />
+          <Route exact path="/profile" component={ Profile } />
+          <Route exact path="/profile/edit" component={ ProfileEdit } />
+          <Route exact path="*" component={ NotFound } />
+        </Switch>
       </div>
     );
   }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,65 @@
 import React from 'react';
+import Loading from '../components/Loading';
+import { createUser } from '../services/userAPI';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      inputName: '',
+      enableButton: true,
+      loading: false,
+    };
+  }
+
+  handleTextInputChange = ({ target }) => {
+    const userName = target.value;
+    const minCharactersInputName = 3;
+    this.setState({
+      inputName: userName,
+    });
+    if (userName.length >= minCharactersInputName) {
+      this.setState({ enableButton: false });
+    } else {
+      this.setState({ enableButton: true });
+    }
+  }
+
+  getUser = async () => {
+    const { inputName } = this.state;
+    const { history } = this.props;
+    console.log(history);
+    this.setState({ loading: true });
+    await createUser({ name: inputName });
+    history.push('/search');
+  }
+
   render() {
-    return (<div data-testid="page-login">Login</div>);
+    const { inputName, enableButton, loading } = this.state;
+    return (
+      <div data-testid="page-login">
+        {loading && <Loading />}
+        <label htmlFor="input-login">
+          Login
+          <input
+            name="login"
+            id="input-login"
+            type="text"
+            data-testid="login-name-input"
+            onChange={ this.handleTextInputChange }
+            value={ inputName }
+          />
+        </label>
+        <button
+          data-testid="login-submit-button"
+          type="button"
+          disabled={ enableButton }
+          onClick={ this.getUser }
+        >
+          Entrar
+        </button>
+      </div>
+    );
   }
 }
 
